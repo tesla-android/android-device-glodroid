c70571be30d5b24e6586a4357f5c8c720b46e1ef
Michał Gapiński <mike@gapinski.eu>
2023-01-14 20:28:51 +0100

    Updated runtime resource overlays

--- common/device-common.mk
+++ common/device-common.mk
@@ -314,5 +314,7 @@ PRODUCT_BUILD_SUPER_PARTITION := true
 PRODUCT_PACKAGES += \
     glodroid_overlay_frameworks_base_core \
     glodroid_overlay_settings_provider \
+    glodroid_overlay_systemui \
+    glodroid_overlay_service_wifi_resources \
 
 PRODUCT_SOONG_NAMESPACES += device/glodroid external/v4l2_codec2
